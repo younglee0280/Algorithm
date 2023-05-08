ddf0607d692d5be5906758edb851612b6d6d1716
younglee0280 <younglee0280@gmail.com>
2023-05-09 00:39:53 +0900

    third

--- 01/test.py
+++ 01/test.py
@@ -1,7 +1,7 @@
-a = int(input('숫자 입력하세요. : '))
-x = int(input('숫자 입력하세요.(2) : '))
-y = int(input('숫자 입력하세요.(3) : '))
-c = int(input('숫자 입력하세요.(4) : '))
+a = int(input('숫자 입력하세요. (a) : '))
+x = int(input('숫자 입력하세요. (x) : '))
+y = int(input('숫자 입력하세요. (y) : '))
+c = int(input('숫자 입력하세요. (c) : '))
 
 a = x if x > y else y #a if b else c: b를 평가한 값이 참이면 a, 거짓이면 c
-print('c는 0입니다.' if c==0 else 'c는 0이 아닙니다.')
+print('(c)는 0입니다.' if c==0 else '(c)는 0이 아닙니다.')
