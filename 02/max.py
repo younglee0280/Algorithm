aca256902bd43dfc9af176fb652f3374df5e41e3
younglee0280 <younglee0280@gmail.com>
2023-05-17 23:57:56 +0900

    2023/05/17 - 1

--- 02/max.py
+++ 02/max.py
@@ -8,11 +8,10 @@ def max_of(a: Sequence) -> Any:
     return maximum
 
 if __name__ == '__main__':
-    print('배열의 최댓값을 구합니다.')
-    num = int(input('원소 수를 입력하세요.'))
+    num = int(input('배열의 최댓값을 구합니다. 원소 수를 입력하세요. '))
     x = [None] * num
 
     for i in range(num):
-        x[i] = int(input(f'x[{i}]값을 입력하세요.'))
+        x[i] = int(input(f'x[{i}]값을 입력하세요. '))
 
     print(f'최댓값은 {max_of(x)}입니다.')
