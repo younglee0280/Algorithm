e82ed45f7edfabaaf12b762f1253ba14074ee490
younglee0280 <younglee0280@gmail.com>
2023-05-21 00:00:02 +0900

    2023/05/20

---
+++ 02/card_conv2.py
@@ -0,0 +1,10 @@
+if __name == '__main__':
+    print('10진수를 n진수로 변환합니다. ')
+
+    while True:
+        while True :
+            no = int(input('변환할 값으로 음이 아닌 정수를 입력하세요. '))
+            if no > 0:
+                break
+        while True : 
+            
