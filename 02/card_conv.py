1e3e2b6d0285fb3815c669ebeb774b770b0d3833
younglee0280 <younglee0280@gmail.com>
2023-05-25 23:39:37 +0900

    2023/05/25

--- 02/card_conv.py
+++ 02/card_conv.py
@@ -8,3 +8,14 @@ def card_conv(x:int, r:int) -> str:
         x //= r 
 
     return d[::-1]
+
+if __name__ == '__main__':
+    print('10진수를 n진수로 변환합니다.')
+
+    while True:
+        while True:
+            no = int(input('변환할 값으로 음이 아닌 정수를 입력하세요. :'))
+            if no > 0:
+                break
+        
+        while True:
