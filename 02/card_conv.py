356d89061e3353afd9306bc324e27915b2a88842
younglee0280 <younglee0280@gmail.com>
2023-05-25 23:41:59 +0900

    2023/05/25

--- 02/card_conv.py
+++ 02/card_conv.py
@@ -19,3 +19,12 @@ if __name__ == '__main__':
                 break
         
         while True:
+            cd = int(input('어떤 진수로 변환할까요? :')
+            if 2 <= cd <= 36:
+                break
+        
+        print(f'{cd}진수로는 {card_conv(no, cd)}입니다.')
+
+        retry - input("한 번 더 변환할까요? (Y ... 예 / N ... 아니오): ")
+        if retry in {'N', 'n'}:
+            break
